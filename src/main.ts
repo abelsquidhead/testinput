2569087920bbbd974e01298aba0ff844b597fbba
abel <abel.wang@gmail.com>
2019-09-06 19:48:57 -0700

    commit

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import * as core from '@actions/core';
 async function run() {
   try {
     const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+
+    console.log("my input: " + myInput);
   } catch (error) {
     core.setFailed(error.message);
   }
